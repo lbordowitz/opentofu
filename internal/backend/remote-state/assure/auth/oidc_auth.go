032d445ac676a100301ca8db07aa1b30f1c5ae4e
Larry Bordowitz <laurence.bordowitz@gmail.com>
2025-07-07 23:48:55 -0500

    first draft implementation of oidc auth in azure

--- internal/backend/remote-state/assure/auth/oidc_auth.go
+++ internal/backend/remote-state/assure/auth/oidc_auth.go
@@ -2,8 +2,13 @@ package auth
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"os"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/opentofu/opentofu/internal/httpclient"
 	"github.com/opentofu/opentofu/internal/tfdiags"
 )
 
@@ -15,21 +20,90 @@ type OIDCAuthConfig struct {
 	OIDCRequestToken  string
 }
 
-// TODO azidentity.NewClientAssertionCredential() -> OIDC, plus OIDC via token request
-// This auth method is probably going to be the most difficult.
-
 type oidcAuth struct{}
 
 func (cred *oidcAuth) Construct(ctx context.Context, config *Config) (azcore.TokenCredential, error) {
-	return nil, nil
+	client := httpclient.New(ctx)
+	token, err := consolidateToken(config.OIDCAuthConfig)
+	if err != nil {
+		return nil, err
+	}
+	return azidentity.NewClientAssertionCredential(
+		config.TenantID,
+		config.ClientID,
+		// TODO use a separate function for obtaining an access token with request url and request token
+		func(innerContext context.Context) (string, error) {
+			return token, nil
+		},
+		&azidentity.ClientAssertionCredentialOptions{
+			ClientOptions: clientOptions(client),
+		},
+	)
 }
+
+func consolidateToken(config *OIDCAuthConfig) (string, error) {
+	token := config.OIDCToken
+	if config.OIDCTokenFilePath != "" {
+		// read token from file. Use as token if provided is empty, or check that they're the same
+		f, err := os.Open(config.OIDCTokenFilePath)
+		if err != nil {
+			return "", fmt.Errorf("error opening token file: %w", err)
+		}
+		defer f.Close()
+		b, err := io.ReadAll(f)
+		if err != nil {
+			return "", fmt.Errorf("error reading token file: %w", err)
+		}
+		file_token := string(b)
+		if token != "" && token != file_token {
+			return "", fmt.Errorf("token provided directly and through file do not match; either make them the same value or only provide one")
+		}
+		token = file_token
+	}
+	return token, nil
+}
+
 func (cred *oidcAuth) Validate(config *Config) tfdiags.Diagnostics {
 	var diags tfdiags.Diagnostics
-	diags = diags.Append(tfdiags.Sourceless(
-		tfdiags.Error,
-		"OIDC Auth is unimplemented",
-		"This authentication method has yet to be implemented",
-	))
+	if !config.UseOIDC {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Use OIDC is not set",
+			"In order to use OpenID Connect credentials, use_oidc or the environment variable ARM_USE_OIDC must be set to true",
+		))
+	}
+	if config.TenantID == "" {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Tenant ID is empty",
+			"In order to use OpenID Connect credentials, a tenant ID is necessary",
+		))
+	}
+	if config.ClientID == "" {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Client ID is empty",
+			"In order to use OpenID Connect credentials, a client ID is necessary",
+		))
+	}
+	/*
+		TODO Validate for oidc_request_url and oidc_request_token
+		This will change how the below diagnostic works
+	*/
+	if config.OIDCToken == "" && config.OIDCTokenFilePath == "" {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Both OIDC Token and OIDC Token File Path are empty",
+			"In order to use OpenID Connect credentials, the access token must be provided, either directly or through a file",
+		))
+	}
+	if _, err := consolidateToken(config.OIDCAuthConfig); err != nil {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"There was a problem reconciling tokens",
+			fmt.Sprintf("In order to use OpenID Connect credentials, the access token provided must be readable and consistent, but the following error was encountered: %s", err.Error()),
+		))
+	}
 	return diags
 }
 
